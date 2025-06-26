f855ebc124cef676a95cd9687b954a8a48e0510b
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-26 20:03:29 +0530

    Add the button.

--- resources/frontend/src/App.tsx
+++ resources/frontend/src/App.tsx
@@ -5,9 +5,10 @@ const App = () => {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
+          <button type="button"
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark
+          </button>
+
       </>
   )
 }
